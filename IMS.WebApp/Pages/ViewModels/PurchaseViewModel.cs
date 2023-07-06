464dbf4e24377aacc373615acaade96e4ca6a9b6
Paul Adamski <pauladamski1985@gmail.com>
2023-07-06 16:47:21 +0200

    update1.08

---
+++ IMS.WebApp/Pages/ViewModels/PurchaseViewModel.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace IMS.WebApp.Pages.ViewModels
+{
+    public class PurchaseViewModel
+    {
+        [Required]
+        public string PONumber { get; set; } = string.Empty;
+        [Required]
+        [Range(minimum:1, maximum:int.MaxValue, ErrorMessage ="You have to select an inventory")]
+        public int InventoryId { get; set; }
+        [Required]
+        [Range(minimum: 1, maximum: int.MaxValue, ErrorMessage = "Quantity has to be greater than 1")]
+        public int QuantityToPurchase { get; set; }
+        [Required]
+        public double InventoryPrice { get; set; }
+
+    }
+}
